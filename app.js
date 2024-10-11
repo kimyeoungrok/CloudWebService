36d575fb68f0c055d8ff308dd4c3123fc1aed561
김영록 <praoo800@konkuk.ac.kr>
2024-10-12 05:50:04 +0900

    ss

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ app.get('/', (req, res) => {
     res.send('hello world');
 })
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
     console.log(`Example app listening on port ${port}`)
 })
