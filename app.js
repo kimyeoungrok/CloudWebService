4a415de97e193f67b2af16b0c15695333d70b7e5
김영록 <praoo800@konkuk.ac.kr>
2024-10-15 19:57:23 +0900

    현재 위치 가져오기

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const path = require('path');
 
 app.get('/', (req, res) => {
     res.send('hello worlds');
 })
 
+app.post('/location', express.json(), (req, res) => {
+    const { latitude, longitude } = req.body;
+    console.log(`Received location: Latitude: ${latitude}, Longitude: ${longitude}`);
+    res.send('Location received');
+})
+
+app.get('/bicycle', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'bicycle.html'));
+});
+
+
 app.listen(port, '0.0.0.0', () => {
     console.log(`Example app listening on port ${port}`)
 })
+
+
